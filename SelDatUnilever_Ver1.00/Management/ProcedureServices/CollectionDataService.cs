3387bc65c2a07e2c50de3d8a3ae0b41ad08927f7
luattran <seldatSWT@gmail.com>
2019-01-04 09:17:48 +0700

    luat edit collectionDataservice 9:17AM 04/01/2019

--- SelDatUnilever_Ver1.00/Management/ProcedureServices/CollectionDataService.cs
+++ SelDatUnilever_Ver1.00/Management/ProcedureServices/CollectionDataService.cs
@@ -16,14 +16,14 @@ using static SelDatUnilever_Ver1._00.Management.DeviceManagement.DeviceItem;
 namespace SelDatUnilever_Ver1
 {
 
- 
+
     public class CollectionDataService
     {
         public enum PalletStatus
         {
-            F=200, // Free pallet
-            W=201, // Have Pallet
-            P=202
+            F = 200, // Free pallet
+            W = 201, // Have Pallet
+            P = 202
 
         }
         //public int planID { get; set; }
@@ -32,10 +32,12 @@ namespace SelDatUnilever_Ver1
         public OrderItem order;
         //public String typeRequest; // FL: ForkLift// BM: BUFFER MACHINE // PR: Pallet return
         //public String activeDate;
-       // public int timeWorkID;
-        public List<Pose> checkInBuffer=new List<Pose>();
+        // public int timeWorkID;
+        public List<Pose> checkInBuffer = new List<Pose>();
         protected BridgeClientRequest clientRequest;
-        public CollectionDataService() {
+        public const String UrlServer = "http://localhost:8081";
+        public CollectionDataService()
+        {
             clientRequest = new BridgeClientRequest();
             clientRequest.ReceiveResponseHandler += ReceiveResponseHandler;
         }
@@ -49,48 +51,22 @@ namespace SelDatUnilever_Ver1
         {
             this.order = order;
         }
-        public String RequestDataProcedure(String dataReq)
+        public String RequestDataProcedure(String dataReq, String url)
         {
-            String url = "http://192.168.1.16:8081/robot/rest/plan/getListPlanPallet";
-           // String url = "http://localhost:8080";
-            var data =clientRequest.PostCallAPI(url, dataReq);
-            if(data.Result!=null)
+            //String url = UrlServer+"/robot/rest/plan/getListPlanPallet";
+            // String url = "http://localhost:8080";
+            var data = clientRequest.PostCallAPI(url, dataReq);
+            if (data.Result != null)
             {
                 return data.Result;
             }
             return null;
         }
-        
+
         public Pose GetCheckInBuffer()
         {
             Pose poseTemp = null;
-            String collectionData=RequestDataProcedure(order.dataRequest);
-            if(collectionData.Length>0)
-            {
-                JArray results = JArray.Parse(collectionData);
-                foreach (var result in results)
-                {
-                    int temp_productDetailID = (int)result["productDetailId"];
-                    if (temp_productDetailID == order.productDetailID)
-                    {
-                        var bufferResults = result["buffers"][0];
-                        String checkinResults =(String) bufferResults["bufferCheckIn"];
-                        JObject stuff = JObject.Parse(checkinResults);
-                        double x = (double)stuff["x"];
-                        double y = (double)stuff["y"];
-                        double angle = (double)stuff["angle"];
-                        poseTemp = new Pose(x, y, angle * Math.PI / 180.0);
-                        break;
-                     
-                    }
-                }
-            }
-            return poseTemp;
-        }
-        public Pose GetFrontLineBuffer()
-        {
-            Pose poseTemp = null;
-            String collectionData = RequestDataProcedure(order.dataRequest);
+            String collectionData = RequestDataProcedure(order.dataRequest, UrlServer + "/robot/rest/plan/getListPlanPallet");
             if (collectionData.Length > 0)
             {
                 JArray results = JArray.Parse(collectionData);
@@ -100,7 +76,33 @@ namespace SelDatUnilever_Ver1
                     if (temp_productDetailID == order.productDetailID)
                     {
                         var bufferResults = result["buffers"][0];
-                        var palletInfo= bufferResults["pallets"][0];
+                        String checkinResults = (String)bufferResults["bufferCheckIn"];
+                        JObject stuff = JObject.Parse(checkinResults);
+                        double x = (double)stuff["x"];
+                        double y = (double)stuff["y"];
+                        double angle = (double)stuff["angle"];
+                        poseTemp = new Pose(x, y, angle * Math.PI / 180.0);
+                        break;
+
+                    }
+                }
+            }
+            return poseTemp;
+        }
+        public Pose GetFrontLineBuffer()
+        {
+            Pose poseTemp = null;
+            String collectionData = RequestDataProcedure(order.dataRequest, UrlServer + "/robot/rest/plan/getListPlanPallet");
+            if (collectionData.Length > 0)
+            {
+                JArray results = JArray.Parse(collectionData);
+                foreach (var result in results)
+                {
+                    int temp_productDetailID = (int)result["productDetailId"];
+                    if (temp_productDetailID == order.productDetailID)
+                    {
+                        var bufferResults = result["buffers"][0];
+                        var palletInfo = bufferResults["pallets"][0];
                         JObject stuff = JObject.Parse((String)palletInfo["dataPallet"]);
                         double x = (double)stuff["line"]["x"];
                         double y = (double)stuff["line"]["y"];
@@ -112,7 +114,7 @@ namespace SelDatUnilever_Ver1
                 }
             }
             return poseTemp;
-            return poseTemp;
+            //return poseTemp;
         }
 
         /*public void GetCheckInBuffer()
@@ -143,7 +145,7 @@ namespace SelDatUnilever_Ver1
         //     JArray results = JArray.Parse(order.dataRequest);
         //     foreach (var result in results)
         //     {
-                
+
         //         int temp_productDetailID = (int)result["productDetailId"];
         //         if (temp_productDetailID == order.productDetailID)
         //         {
@@ -165,7 +167,7 @@ namespace SelDatUnilever_Ver1
         //                 PointDetect pointDet = new PointDetect(new Point(pX, pY),(TrafficRobotUnity.MvDirection)pDBmvDir);
         //                 tempPDB = new PointDetectBranching() { xy = pointDet,brDir=(TrafficRobotUnity.BrDirection)pDBbrvDir };
         //             }
-                    
+
         //             /* var ppalets = dataPalletItemResults["pallet"];
         //              double ppX = (double)ppalets["point"]["X"];
         //              double ppY = (double)ppalets["point"]["Y"];
@@ -209,15 +211,45 @@ namespace SelDatUnilever_Ver1
             return order.palletAtMachine.linePos;
         }
 
-        public Pose GetCheckInReturn()
+        public Pose GetCheckInReturn(TrafficRobotUnity.PistonPalletCtrl pisCtrl)
         {
+            dynamic product = new JObject();
+            product.palletStatus = pisCtrl;
             Pose poseTemp = null;
+            String collectionData = RequestDataProcedure(product.ToString(), UrlServer + "/robot/rest/buffer/getListBufferReturn");
+            if (collectionData.Length > 0)
+            {
+                JArray results = JArray.Parse(collectionData);
+                var bufferResults = results[0];
+                String checkinResults = (String)bufferResults["bufferCheckIn"];
+                JObject stuff = JObject.Parse(checkinResults);
+                double x = (double)stuff["x"];
+                double y = (double)stuff["y"];
+                double angle = (double)stuff["angle"];
+                poseTemp = new Pose(x, y, angle * Math.PI / 180.0);
+            }
             return poseTemp;
         }
 
-        public Pose GetFrontLineReturn()
+        public Pose GetFrontLineReturn(TrafficRobotUnity.PistonPalletCtrl pisCtrl)
         {
+
             Pose poseTemp = null;
+            dynamic product = new JObject();
+            product.palletStatus = pisCtrl;
+            String collectionData = RequestDataProcedure(product.ToString(), UrlServer + "/robot/rest/buffer/getListBufferReturn");
+            if (collectionData.Length > 0)
+            {
+                JArray results = JArray.Parse(collectionData);
+                var bufferResults = results[0];
+                var palletInfo = bufferResults["pallets"][0];
+                JObject stuff = JObject.Parse((String)palletInfo["dataPallet"]);
+                double x = (double)stuff["line"]["x"];
+                double y = (double)stuff["line"]["y"];
+                double angle = (double)stuff["line"]["angle"];
+                poseTemp = new Pose(x, y, angle * Math.PI / 180.0);
+
+            }
             return poseTemp;
         }
 
@@ -226,7 +258,7 @@ namespace SelDatUnilever_Ver1
         public String GetInfoOfPalletBuffer(TrafficRobotUnity.PistonPalletCtrl pisCtrl)
         {
             JInfoPallet infoPallet = new JInfoPallet();
-            String collectionData = RequestDataProcedure(order.dataRequest);
+            String collectionData = RequestDataProcedure(order.dataRequest, UrlServer + "/robot/rest/plan/getListPlanPallet");
             if (collectionData.Length > 0)
             {
                 JArray results = JArray.Parse(collectionData);
@@ -235,7 +267,7 @@ namespace SelDatUnilever_Ver1
                     int temp_productDetailID = (int)result["productDetailId"];
                     if (temp_productDetailID == order.productDetailID)
                     {
-                        
+
 
                         var bufferResults = result["buffers"][0];
                         var palletInfo = bufferResults["pallets"][0];
@@ -272,29 +304,42 @@ namespace SelDatUnilever_Ver1
         public String GetInfoOfPalletReturn(TrafficRobotUnity.PistonPalletCtrl pisCtrl)
         {
             JInfoPallet infoPallet = new JInfoPallet();
+            dynamic product = new JObject();
+            product.palletStatus = pisCtrl;
+            String collectionData = RequestDataProcedure(product.ToString(), UrlServer + "/robot/rest/buffer/getListBufferReturn");
+            if (collectionData.Length > 0)
+            {
+                JArray results = JArray.Parse(collectionData);
+                var bufferResults = results[0];
+                var palletInfo = bufferResults["pallets"][0];
+                JObject stuff = JObject.Parse((String)palletInfo["dataPallet"]);
+                int row = (int)stuff["pallet"]["row"];
+                int bay = (int)stuff["pallet"]["bay"];
+                int direct = (int)stuff["pallet"]["direct"];
 
-            infoPallet.pallet = pisCtrl; /* dropdown */
-            infoPallet.bay = 1;
-            infoPallet.hasSubLine = "yes"; /* no */
-            infoPallet.direction = TrafficRobotUnity.BrDirection.FORWARD; /* right */
-            infoPallet.row = 2;
+                infoPallet.pallet = pisCtrl; /* dropdown */
+                infoPallet.bay = bay;
+                infoPallet.hasSubLine = "yes"; /* no */
+                infoPallet.direction = (TrafficRobotUnity.BrDirection)direct; /* right */
+                infoPallet.row = row;
 
+            }
             return JsonConvert.SerializeObject(infoPallet);
         }
 
         public void UpdatePalletState(PalletStatus palletStatus)
         {
-            String url = "http://192.168.1.17:8081/robot/rest/pallet/updatePalletStatus";
+            String url = UrlServer+"/robot/rest/pallet/updatePalletStatus";
             dynamic product = new JObject();
             product.palletId = order.palletId;
             product.palletStatus = palletStatus.ToString();
             product.updUsrId = order.updUsrId;
-            var data=clientRequest.PostCallAPI(url, product.ToString());
+            var data = clientRequest.PostCallAPI(url, product.ToString());
             if (data.Result == null)
             {
                 ErrorHandler(ProcedureMessages.ProcMessage.MESSAGE_ERROR_UPDATE_PALLETSTATUS);
             }
-         }
+        }
         protected virtual void ReceiveResponseHandler(String msg) { }
         protected virtual void ErrorHandler(ProcedureMessages.ProcMessage procMessage) { }
     }
